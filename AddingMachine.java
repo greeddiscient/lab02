f8dcda3b0640946a6522c4b3ccc8a4a5b6e91ea0
Shaun Djuhari <shaundjuhari@berkeley.edu>
2015-09-12 01:47:29 -0700

    initial

---
+++ AddingMachine.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class AddingMachine {
+
+	public static void main (String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		boolean justStarting = true;
+		int total = 0;
+		int subtotal = 0;
+		while (true) {
+		    // TODO Your code here
+		}
+	}
+
+}
